08553e622eb3546ccf7240fb113da3e737fec043
Marcin Winiarski <winiardesign@gmail.com>
2023-07-08 21:28:34 +0200

    add fetch data from server

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-// import { notFound } from "next/navigation";
+import { notFound } from "next/navigation";
+
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { cache: 'no-store' })
+  if (!res.ok) {
+    return notFound();
+  }
+  return res.json()
+}
 
 
 const BlogPost = async ({ params }) => {
- 
+ const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>tytuł</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            inna treść
+          {data.body}
           </p>
           <div className={styles.author}>
             <Image
@@ -22,7 +30,7 @@ const BlogPost = async ({ params }) => {
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Username</span>
+            <span className={styles.username}>{data.userId}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
@@ -36,7 +44,14 @@ const BlogPost = async ({ params }) => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-         tutaj będzie treść
+         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis consequatur accusamus, nihil adipisci aliquid recusandae eveniet! Inventore est voluptatem, voluptatibus voluptate vero ipsam sapiente minus neque? Voluptatem mollitia repellat expedita.
+         Nesciunt sapiente magnam neque accusamus, a eaque quas accusantium temporibus esse praesentium fugit quos ipsam earum, corporis incidunt, veniam sunt. Harum maiores cumque hic vero minus ipsam obcaecati totam architecto.
+         Dolor fugit enim veritatis vitae suscipit quasi reprehenderit natus, laudantium earum nihil, molestiae vel! Error architecto nulla tempora, magnam quam dolor neque quae assumenda quidem impedit beatae quod. Accusamus, deleniti.
+         <br/>
+         <br/>
+          <p>Nesciunt sapiente magnam neque accusamus, a eaque quas accusantium temporibus esse praesentium fugit quos ipsam earum, corporis incidunt, veniam sunt. Harum maiores cumque hic vero minus ipsam obcaecati totam architecto.
+          Dolor fugit enim veritatis vitae suscipit quasi reprehenderit natus, laudantium earum nihil, molestiae vel! Error architecto nulla tempora, magnam quam dolor neque quae assumenda quidem impedit beatae quod. Accusamus, deleniti.
+          </p>
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,27 +2,58 @@ import Link from 'next/link';
 import styles from './page.module.css';
 import Image from 'next/image';
 
-function Blog() {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+async function Blog() {
+  const data = await getData();
+  let data2 = [];
+  for(let i = 0 ; i < 10; i++) {
+    data2.push(data[i]);
+  }
+
+  const images = [
+    { 
+      id: 0,
+      url: "https://images.pexels.com/photos/2422259/pexels-photo-2422259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    { 
+      id: 1,
+      url: "https://images.pexels.com/photos/346885/pexels-photo-346885.jpeg"
+    },
+    { 
+      id: 2,
+      url: "https://images.pexels.com/photos/586687/pexels-photo-586687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    { 
+      id: 3,
+      url: "https://images.pexels.com/photos/3889987/pexels-photo-3889987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    { 
+      id: 4,
+      url: "https://images.pexels.com/photos/2086925/pexels-photo-2086925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+  ]
+  const defaultImage = "https://images.pexels.com/photos/2901209/pexels-photo-2901209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testID" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image src="https://images.pexels.com/photos/2132126/pexels-photo-2132126.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="400" height="250" className={styles.image}/>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testID" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image src="https://images.pexels.com/photos/2132126/pexels-photo-2132126.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="400" height="250" className={styles.image}/>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data2.map((item, index) => (
+        <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image src={(index < 5) ? images[index].url : defaultImage} alt="" width="400" height="250" className={styles.image}/>
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
