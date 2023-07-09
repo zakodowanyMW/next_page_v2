605aad8c0a204cdf06f153802ad0cb3a8babadaf
Marcin Winiarski <winiardesign@gmail.com>
2023-07-09 08:03:29 +0200

    add scrollbar styles

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -13,28 +13,28 @@ async function getData() {
 async function Blog() {
   const data = await getData();
   let data2 = [];
-  for(let i = 0 ; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     data2.push(data[i]);
   }
 
   const images = [
-    { 
+    {
       id: 0,
       url: "https://images.pexels.com/photos/2422259/pexels-photo-2422259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
-    { 
+    {
       id: 1,
       url: "https://images.pexels.com/photos/346885/pexels-photo-346885.jpeg"
     },
-    { 
+    {
       id: 2,
       url: "https://images.pexels.com/photos/586687/pexels-photo-586687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
-    { 
+    {
       id: 3,
       url: "https://images.pexels.com/photos/3889987/pexels-photo-3889987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
-    { 
+    {
       id: 4,
       url: "https://images.pexels.com/photos/2086925/pexels-photo-2086925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
@@ -46,7 +46,7 @@ async function Blog() {
       {data2.map((item, index) => (
         <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
-            <Image src={(index < 5) ? images[index].url : defaultImage} alt="" width="400" height="250" className={styles.image}/>
+            <Image src={(index < 5) ? images[index].url : defaultImage} alt="" width="400" height="250" className={styles.image} />
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
